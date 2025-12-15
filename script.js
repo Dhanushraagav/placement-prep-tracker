7a2eb35d67df698bcb3b2ba045f1543b9951aa17
Dhanushraagav S <dhanushraagav01@gmail.com>
2025-12-15 16:39:26 +0530

    Added basic UI and topic add functionality

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function addTopic() {
+  const input = document.getElementById("topicInput")
+  const topic = input.value.trim()
 
+  if (topic === "") return
+
+  const li = document.createElement("li")
+  li.innerText = topic
+
+  document.getElementById("topicList").appendChild(li)
+  input.value = ""
+}
